38ecbaebb927f0f0e4ab82f2d740fb1e2d6a2a40
mundane <799699348@qq.com>
2019-06-26 08:48:30 +0800

    电子手账

--- DianZiShouZhang/app/src/main/java/com/example/myapp/Map.java
+++ DianZiShouZhang/app/src/main/java/com/example/myapp/Map.java
@@ -2,6 +2,7 @@ package com.example.myapp;
 
 import android.os.Bundle;
 import android.view.Window;
+
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
@@ -9,39 +10,42 @@ import com.example.base.BaseActivity;
 
 public class Map extends BaseActivity {
 
-	private MapView mMapView=null;
-	private BaiduMap mBaiduMap;
-	private void init() {
-		mMapView = (MapView)findViewById(R.id.bmapview);
-		mBaiduMap=mMapView.getMap();
-		mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
-	}
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		SDKInitializer.initialize(getApplicationContext());
-		setContentView(R.layout.map);
-		init();
-	}
-	@Override
-	protected void onDestroy() {
-		mMapView.onDestroy();
-		mMapView=null;
-		super.onDestroy();
-	}
-	@Override
-	protected void onPause() {
-		super.onPause();
-		mMapView.onPause();
-	}
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mMapView.onResume();
-	}
-	
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap;
+
+    private void init() {
+        mMapView = (MapView) findViewById(R.id.bmapview);
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.map);
+        init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
 
 
-	
 }
